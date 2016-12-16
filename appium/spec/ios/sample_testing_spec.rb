b94a0c14df904b4f8c6f75a0a29c1a7e6d957be9
DGG <dongcheng01@chinatopcredit.com>
2016-12-16 12:49:55 +0800

    appium ios demo

---
+++ appium/spec/ios/sample_testing_spec.rb
@@ -0,0 +1,56 @@
+require 'appium_lib'
+
+RSpec.describe 'iOS app testing' do
+  context "try it" do
+    it 'init ios webdriver' do
+      # singleton mode
+      return if $driver
+
+      # # define driver info in desired_capabilities
+      # APP_PATH = 'app/UICatalog6.1.app.zip'
+      # desired_capabilities = {
+      #   caps: {
+      #     platformName: 'iOS',
+      #     versionNumber: '10.1',
+      #     deviceName: 'iPhone 6',
+      #     app: APP_PATH,
+      #     automationName: 'XCUITest',
+      #   },
+      #   appium_lib: {
+      #     sauce_name: nil,
+      #     sauce_access_key: nil,
+      #   }
+      # }
+
+      # init driver from appium.txt
+      caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'appium.txt')
+      # new the driver
+      Appium::Driver.new(caps)
+    end
+
+    it 'launch ios driver' do
+      $driver.start_driver
+    end
+
+    it 'click a button then Back one by one' do
+      cells = $driver.texts('uses')
+      cells.each do |cell|
+        cell.click
+        $driver.back
+      end
+    end
+
+    it 'using Appium.promote_appium_methods to get Page object' do
+      Appium.promote_appium_methods RSpec::ExampleGroups::IOSAppTesting
+      cells = texts('uses')
+      cells.each do |cell|
+        cell.click
+        back
+      end
+    end
+
+    it 'close ios driver' do
+      $driver.driver_quit
+    end
+  end
+end
